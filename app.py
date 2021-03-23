8b2b58c35c313f9c40fc64a552b36bf4363f0f0d
sidkulk <kulksud@gmail.com>
2021-03-23 22:13:48 +0530

    fixed login-page functionality

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 from flask import Flask, render_template, request, session, logging, url_for, redirect, flash
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
 
 from passlib.hash import sha256_crypt
 
 engine = create_engine("sqlite:///test.db")
 db = scoped_session(sessionmaker(bind=engine))
 app = Flask(__name__)
+app.config.update({
+    'SQLALCHEMY_POOL_SIZE': None,
+    'SQLALCHEMY_POOL_TIMEOUT': None
+})
 
-@app.route("/")
-def home():
-    return render_template('home.html')
+app.config['SECRET_KEY'] = 'thisisasecret'
 
 #register
-@app.route('/register', methods=["GET", "POST"])
+@app.route('/page-register', methods=["GET", "POST"])
 def register():
     if request.method == 'POST':
         name = request.form.get("username")
@@ -26,39 +30,49 @@ def register():
             db.execute("INSERT into REGISTER(username, email, password) VALUES(:name, :email, :password)", {"name":name, "email":email, "password":securepass})
             db.commit()
             flash("Account created!", "success")
-            return redirect(url_for('login'))
+            return render_template('page-login.html')
         else:
             flash("password doesn't match", "danger")
-            return render_template("register.html")
-    return render_template('register.html')
+            return render_template("page-register.html")
+    return render_template('page-register.html')
 
 #login
-@app.route('/login', methods=["GET", "POST"])
+@app.route('/page-login', methods=["GET", "POST"])
 def login():
     if request.method == 'POST':
-        username = request.form.get("username")
+        email = request.form.get("email")
         password = request.form.get("password")
 
-        usernamedata = db.execute("SELECT username FROM REGISTER WHERE username=:username", {"username":username}).fetchone()
-        passwordata = db.execute("SELECT password FROM REGISTER WHERE username=:username", {"username":username}).fetchone()
+        usernamedata = db.execute("SELECT email FROM REGISTER WHERE email=:email", {"email":email}).fetchone()
+        passwordata = db.execute("SELECT password FROM REGISTER WHERE email=:email", {"email":email}).fetchone()
 
         if usernamedata is None:
             flash("No user found!", "danger")
-            return render_template("login.html")
+            return render_template("page-login.html")
         else:
             for pass_data in passwordata:
                 if sha256_crypt.verify(password, pass_data):
                     flash("Login successful!", "success")
-                    return redirect(url_for('dash'))
+                    return redirect(url_for("index"))
                 else:
                     flash("Incorrect Password", "danger")
-                    return render_template("login.html")
+                    return render_template("page-login.html")
+    return render_template('page-login.html')
 
-    return render_template('login.html')
+@app.route('/index', methods=["GET", "POST"])
+def index():
+    return render_template("index.html")
 
-@app.route('/dash')
-def dash():
-    return render_template("dash.html")
+@app.route('/page-forgot-password', methods=["GET", "POST"])
+def recovery():
+    if request.method == "POST":
+        email = request.form.get("email")
+        useremail = db.execute("SELECT email FROM REGISTER WHERE email=:email", {"email":email}).fetchone()
+        if useremail == None:
+            return render_template("page-register.html")
+        else:
+            return render_template("index.html")
+    return render_template('page-forgot-password.html')
 
 if __name__ == '__main__':
     app.secret_key="kulksidtestkey"
